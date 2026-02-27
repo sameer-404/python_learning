67dc8ba2356a30197004bb0e5de3539330e623cc
Sameer Yogi <sameeryog99@gmail.com>
2026-02-27 10:46:51 -0500

    Project 01: Mad Libs generator

---
+++ Mad_Libs/mad_libs.py
@@ -0,0 +1,10 @@
+#Let's get about 4 intputs from user and print a random sentence.
+
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+second_name = input("Enter another name: ")
+second_place = input("Enter another place: ")
+
+print(f"{name} went to {place} and it was very {adjective}. Then {name} decided to {verb}. {second_name} went to {second_place}.")
